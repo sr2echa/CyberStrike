f6dbea5f38f0f203260e3a0a11e8c1ee2bbd9a39
Akkilesh-A <akkilalagar05@gmail.com>
2024-10-02 22:12:09 +0530

    responsiveness change

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -57,7 +57,7 @@ export function LandingSection(){
         <CardGrid />
         <BigCard />
       </div>
-      <div>
+      <div className="py-6">
         <p className="text-center font-mono lowercase pb-4">Made with 💖 by <span className="font-extrabold">Team Fischer - </span> <a className="underline" href="https://github.com/sr2echa">Sreecha</a> , <a className="underline" href="https://github.com/wreckage0907">Girish</a> , <a className="underline" href="https://github.com/TobyVincentJohn">Toby</a> & <a className="underline" href="https://github.com/Akkilesh-A">Akkilesh</a></p>
       </div>
     </div>
