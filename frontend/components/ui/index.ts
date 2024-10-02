c161c5a5fe325c86c1af261586bf8e445ee4bee0
Akkilesh-A <akkilalagar05@gmail.com>
2024-10-02 23:48:05 +0530

    remove sparkles component

--- frontend/components/ui/index.ts
+++ frontend/components/ui/index.ts
@@ -3,7 +3,6 @@ import { Card } from "./card";
 import { GlowingStarsBackgroundCard, GlowingStarsDescription,GlowingStarsTitle, } from "./glowing-stars";
 import { Input } from "./input";
 import { Progress } from "./progress";
-import { SparklesCore } from "./sparkles";
 import { HoverEffect } from "./card-hover-effect";
 import { RainbowButton } from "./rainbow-button";
 import { FileUpload } from "./file-upload";
@@ -29,7 +28,6 @@ export {
     Input,
     Card,
     Progress,    
-    SparklesCore,
     HoverEffect,
     RainbowButton,
     FileUpload,
