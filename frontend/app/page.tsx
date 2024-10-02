ad0f3abf43d9750b756c6cb746fa06120041cd3f
Akkilesh-A <akkilalagar05@gmail.com>
2024-10-03 00:26:12 +0530

    push to prod 🛫

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -59,7 +59,7 @@ const LandingSection=()=>{
         <BigCard />
       </div>
       <div className="pt-6 pb-4">
-        <p className="text-center font-mono lowercase pb-4">Made with 💖 by <span className="font-extrabold">Team Fischer</span> <a className="underline" href="https://github.com/sr2echa">Sreecha</a> , <a className="underline" href="https://github.com/wreckage0907">Girish</a> , <a className="underline" href="https://github.com/TobyVincentJohn">Toby</a> & <a className="underline" href="https://github.com/Akkilesh-A">Akkilesh</a></p>
+        <p className="text-center font-mono lowercase pb-4">Made with 💖 by <span className="font-extrabold ">Team Fischer</span> <a className="underline" href="https://github.com/sr2echa">Sreecha</a> , <a className="underline" href="https://github.com/wreckage0907">Girish</a> , <a className="underline" href="https://github.com/TobyVincentJohn">Toby</a> & <a className="underline" href="https://github.com/Akkilesh-A">Akkilesh</a></p>
       </div>
     </div>
   )
